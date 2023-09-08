d5e415b808c5e7746ce76bc40c4df6950bc2bc5c
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-08 16:11:01 +0530

    Initial commit

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php App</title>
+</head>
+<body>
+<?php
+    echo "Hare Krishna <br/> <br/>";
+    echo "Hello World!";
+    ?>   
+</body>
+</html>
